22521f0bd70715791e225baad3cfb1baa16e6b6e
Jacob Sanders <sanders.jms@gmail.com>
2022-06-11 03:36:46 -0600

    added a reduce method to put array into hash

--- substrings.rb
+++ substrings.rb
@@ -5,4 +5,8 @@ def substrings(string, dictionary)
       contained_words << e
     end
   end
+  contained_words.reduce(Hash.new(0)) do |result, occurances|
+    result[occurances] += 1
+    result
+  end
 end 
