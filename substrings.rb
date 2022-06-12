32282c2d54feb988c739f18958a9fb4aacb8b063
Jacob Sanders <sanders.jms@gmail.com>
2022-06-12 01:39:43 -0600

    added code to account for strings with multiple words. Code complete and works

--- substrings.rb
+++ substrings.rb
@@ -1,12 +1,22 @@
+# require 'pry-byebug'
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
 def substrings(string, dictionary)
   contained_words = []
-  dictionary.each do |e|
-    if string.include?(e)
-      contained_words << e
+  string_array_downcase = string.downcase.split(' ')
+
+  dictionary.each do |word|
+    string_array_downcase.each do |elem|
+      if elem.include?(word)
+        contained_words << word
+      end
     end
   end
   contained_words.reduce(Hash.new(0)) do |result, occurances|
     result[occurances] += 1
     result
   end
-end 
+end
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
